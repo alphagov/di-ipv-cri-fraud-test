60d5b82d3b9f8a65161ec58233ac441eeb2b3b60
daniel.flynn <dfflynn@deloitte.co.uk>
2022-06-28 15:37:00 +0100

    KBV-48 - Introduce New Tests

--- src/test/java/gov/di_ipv_fraud/pages/FraudPageObject.java
+++ src/test/java/gov/di_ipv_fraud/pages/FraudPageObject.java
@@ -6,47 +6,109 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.ArrayList;
+
+import static gov.di_ipv_fraud.pages.Headers.CHECKING_YOUR_DETAILS;
+import static gov.di_ipv_fraud.pages.Headers.IPV_CORE_STUB;
+
 
 public class FraudPageObject extends UniversalSteps {
 
     @FindBy(xpath = "//*[@id=\"main-content\"]/p/a/button")
     public WebElement visitCredentialIssuers;
-    @FindBy(xpath = "//*[@id=\"main-content\"]/p[20]/a/input")
+    public static final String IPV_CORE_STUB_ENDPOINT = "ipvCoreStubEndpoint";
+    @FindBy(xpath = "//*[@id=\"main-content\"]/p[7]/a/input")
+    public WebElement fraudCRIBuild;
+    @FindBy(xpath = "//*[@id=\"main-content\"]/p[8]/a/input")
     public WebElement fraudCRIStaging;
     @FindBy(id = "rowNumber")
     public WebElement selectRow;
-    @FindBy(xpath = "//*[@id=\"main-content\"]/form[1]/div/button")
-    public WebElement searchButton;
+    @FindBy(xpath = "//*[@id=\"main-content\"]/p[9]/a/input")
+    public WebElement fraudCRIIntegration;
     @FindBy(xpath = "//*[@id=\"main-content\"]/div/div/form/button")
     public WebElement checkYourDetailsContinue;
     @FindBy(xpath = "//*[@id=\"main-content\"]/div/details/summary/span")
     public WebElement viewResponse;
+    @FindBy(xpath = "//*[@id=\"main-content\"]/form[2]/div/button")
+    public WebElement searchButton;
+    @FindBy(xpath = "//*[@id=\"main-content\"]/div/div/form/details/summary/span")
+    public WebElement whoWeCheckLink;
+    @FindBy(xpath = "//*[@id=\"main-content\"]/div/div/form/details/div/p[1]/a")
+    public WebElement experianLink;
+    @FindBy(xpath = "//*[@id=\"main-content\"]/div/div/form/details/div/p[2]/a")
+    public WebElement privacyPolicyLink;
 
 
     public FraudPageObject() {
         PageFactory.initElements(Driver.get(), this);
     }
 
-
     public void navigateToIPVCoreStub() {
-        Driver.get().get(ConfigurationReader.get("ipvCoreStubEndpoint"));
-        waitForFiveSeconds();
+        Driver.get().get(ConfigurationReader.get(IPV_CORE_STUB_ENDPOINT));
+        waitForTextToAppear(IPV_CORE_STUB);
 
     }
 
-    public void navigateToFraudCRI() {
+    public void navigateToFraudCRI(String environment) {
         visitCredentialIssuers.click();
-        waitForFiveSeconds();
-        fraudCRIStaging.click();
+        assertURLContains("credential-issuers");
+
+        switch (environment) {
+            case "Build":
+                fraudCRIBuild.click();
+                break;
+
+            case "Staging":
+                fraudCRIStaging.click();
+                break;
+
+            case "Integration":
+                fraudCRIIntegration.click();
+                break;
+
+            default:
+                break;
+
+        }
+
     }
 
     public void searchForUATUser(String number) {
-        waitForFiveSeconds();
+        assertURLContains("credential-issuer?cri=fraud-cri-integration");
         selectRow.sendKeys(number);
         searchButton.click();
+
+    }
+
+    public void navigateToResponse() {
+        waitForTextToAppear(CHECKING_YOUR_DETAILS);
         checkYourDetailsContinue.click();
-        waitForFiveSeconds();
+        assertURLContains("callback");
         viewResponse.click();
+    }
+
+    public void whoWeCheckDetailsWith(String page) {
+        waitForTextToAppear(CHECKING_YOUR_DETAILS);
+        whoWeCheckLink.click();
+
+
+        if ("Experian".equalsIgnoreCase(page)) {
+            experianLink.click();
+            ArrayList<String> newTb = new ArrayList<String>(Driver.get().getWindowHandles());
+            Driver.get().switchTo().window(newTb.get(1));
+            assertURLContains("https://www.experian.co.uk/");
+            Driver.get().close();
+            Driver.get().switchTo().window(newTb.get(0));
+
+        } else {
+            privacyPolicyLink.click();
+            ArrayList<String> newTb = new ArrayList<String>(Driver.get().getWindowHandles());
+            Driver.get().switchTo().window(newTb.get(1));
+            assertURLContains("privacy-notice");
+            Driver.get().close();
+            Driver.get().switchTo().window(newTb.get(0));
+        }
+
 
     }
 
--- src/test/java/gov/di_ipv_fraud/pages/Headers.java
+++ src/test/java/gov/di_ipv_fraud/pages/Headers.java
@@ -5,4 +5,7 @@ public class Headers {
     public static final String CHECKING_YOUR_DETAILS = "Checking your details";
     public static final String IPV_CORE_STUB = "IPV Core Stub";
     public static final String CREDENTIAL_ISSUERS = "Visit Credential Issuers";
+    public static final String PROBLEM_WITH_THE_SERVICE = "Sorry, there is a problem with the service";
+    public static final String FRAUD_CRI_ENVIRONMENT = "User for Fraud CRI";
+    public static final String VERIFIABLE_CREDENTIALS = "Verifiable Credentials";
 }
